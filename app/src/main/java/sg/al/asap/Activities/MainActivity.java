21137c63b98376e73541bc10b788f48a316ff16c
Ibrahim Alrasyid Waterkamp <s10188377@connect.np.edu.sg>
2019-07-24 00:40:02 +0800

    - Added user fragment - Get name from firebase is still not working - Get Email is working - Updated UI

--- app/src/main/java/sg/al/asap/Activities/MainActivity.java
+++ app/src/main/java/sg/al/asap/Activities/MainActivity.java
@@ -6,9 +6,14 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import sg.al.asap.Fragments.CartFragment;
 import sg.al.asap.DarkModePrefManager;
@@ -21,6 +26,11 @@ public class MainActivity extends AppCompatActivity /*implements NavigationView.
 
     private FirebaseAuth mAuth;
     private BottomNavigationView bottomNavigationView;
+    FirebaseUser currentuser;
+
+    ImageView profilePic;
+    TextView userName ;
+    TextView userEmail ;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -43,8 +53,8 @@ public class MainActivity extends AppCompatActivity /*implements NavigationView.
                     selectedFragment = new CartFragment();
                     break;
 //                case  R.id.navigationMenu:
-////                    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-////                    drawer.openDrawer(GravityCompat.START);
+//                    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//                    drawer.openDrawer(GravityCompat.START);
 //                    selectedFragment = new MenuFragment();
 //                    break;
             }
@@ -59,6 +69,7 @@ public class MainActivity extends AppCompatActivity /*implements NavigationView.
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+        currentuser = mAuth.getCurrentUser();
 
         if(new DarkModePrefManager(this).isNightMode()){
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
@@ -70,6 +81,23 @@ public class MainActivity extends AppCompatActivity /*implements NavigationView.
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         bottomNavigationView.setSelectedItemId(R.id.navigationHome);
+    }
+
+    public void update(){
+         profilePic = (ImageView) findViewById(R.id.profileCircleImageView);
+         userName = (TextView) findViewById(R.id.usernameTextView);
+         userEmail = (TextView) findViewById(R.id.userEmail);
+
+        if(currentuser.getEmail() == null || currentuser.getDisplayName() == null){
+          userName.setText("User not found");
+        }
+        else{
+            userEmail.setText(currentuser.getEmail());
+            userName.setText(currentuser.getDisplayName());
+        }
+
+        //use glide(library) to load user image
+        Glide.with(this).load(currentuser.getPhotoUrl()).into(profilePic);
 
     }
 
--- app/src/main/java/sg/al/asap/Fragments/UserFragment.java
+++ app/src/main/java/sg/al/asap/Fragments/UserFragment.java
@@ -1,25 +1,68 @@
 package sg.al.asap.Fragments;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
+import sg.al.asap.Activities.MainActivity;
 import sg.al.asap.Activities.SignupActivity;
 import sg.al.asap.R;
 
 public class UserFragment extends Fragment {
+
+    FirebaseAuth mAuth;
+    FirebaseUser currentuser;
+
     @Nullable
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
 
-
-        return inflater.inflate(R.layout.fragment_user, container,false);
+        return view;
 
     }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // do your variables initialisations here except Views!!!
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        // initialise your views
+        ImageView profilePic = (ImageView) view.findViewById(R.id.profileCircleImageView);
+        TextView userName = (TextView) view.findViewById(R.id.usernameTextView);
+        TextView userEmail = (TextView) view.findViewById(R.id.userEmail);
+
+        Switch darkModeSwitch = (Switch) view.findViewById(R.id.darkModeSwitch);
+        TextView editProfile = (TextView) view.findViewById(R.id.editProf);
+        TextView changePass = (TextView) view.findViewById(R.id.changePassword);
+        TextView notification = (TextView) view.findViewById(R.id.notification);
+        TextView logOut = (TextView) view.findViewById(R.id.logOut);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentuser = mAuth.getCurrentUser();
+
+        ((MainActivity)getActivity()).update();
+    }
+
+
+
 }
