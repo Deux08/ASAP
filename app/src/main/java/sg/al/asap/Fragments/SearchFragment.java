57d2c9bc4e392c3bc473af5a644dc76568c425bb
Sean Nicholas <seannts9@gmail.com>
2019-07-24 15:07:40 +0800

    created working search function

--- app/src/main/java/sg/al/asap/Fragments/SearchFragment.java
+++ app/src/main/java/sg/al/asap/Fragments/SearchFragment.java
@@ -7,13 +7,70 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
 
 import sg.al.asap.R;
 
 public class SearchFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_search, container,false);
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ArrayList<String> avengers = new ArrayList<>();
+        avengers.add("Iron Man");
+        avengers.add("Captain America");
+        avengers.add("Vision");
+        avengers.add("Hawk Eye");
+        avengers.add("Ant-Man");
+        avengers.add("Spider-Man");
+        avengers.add("Captain Marvel");
+        avengers.add("Doctor Strange");
+        avengers.add("Black Panther");
+        avengers.add("Hulk");
+        avengers.add("Star Lord");
+        avengers.add("Rocket");
+        avengers.add("Groot");
+        avengers.add("Thor");
+        avengers.add("Quicksilver");
+        avengers.add("Scarlet Witch");
+        avengers.add("Falcon");
+        avengers.add("War Machine");
+
+        SearchView searchBar = (SearchView) view.findViewById(R.id.searchBar);
+        ListView searchList = (ListView) view.findViewById(R.id.searchList);
+
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(view.getContext(), R.layout.search_listview, R.id.search_listview, avengers);
+        searchList.setAdapter(adapter);
+
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+    }
+
+
 }
