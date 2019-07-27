de40f748dc634ac5d00db7a05a767a22cac383c5
Adelollipop <38975892+Adelollipop@users.noreply.github.com>
2019-07-27 22:06:19 +0800

    adding the fragment for the product page and editing the home page

--- app/src/main/java/sg/al/asap/Fragments/HomeFragment.java
+++ app/src/main/java/sg/al/asap/Fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package sg.al.asap.Fragments;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -32,8 +33,13 @@ public class HomeFragment extends Fragment {
         listView = view.findViewById(R.id.galleryLV);
 
 
-        productslist.add(new Product("gucci bag", 3));
-        productslist.add(new Product("bag", 5));
+        productslist.add(new Product("Gucci bag", 3));
+        productslist.add(new Product("Nike shoes", 10));
+        productslist.add(new Product("FIla bag", 9));
+        productslist.add(new Product("Chanel perfume", 6));
+        productslist.add(new Product("Unik shirt", 5));
+        productslist.add(new Product("dog", 11));
+
 
         ProductAdapter adapter = new ProductAdapter(view.getContext(), productslist);
         listView.setAdapter(adapter);
@@ -43,6 +49,12 @@ public class HomeFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Uri uri);
     }
+//    void showlistview (){
+//
+//        Intent intent = new Intent(HomeFragment.this);
+//        startActivity(intent);
+//
+//    }
 
 
 //    public View onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState, @NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
---
+++ app/src/main/java/sg/al/asap/Fragments/ProductFragment.java
@@ -0,0 +1,19 @@
+package sg.al.asap.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import sg.al.asap.R;
+
+public class ProductFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_product, container,false);
+    }
+}
